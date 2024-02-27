83ef2c0645825d8a39ee628315b307773700b2a5
amadich <jeridi.moha@gmail.com>
2024-02-27 22:50:00 +0100

    RPG-text-v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,48 @@ export default function Home() {
           alt="Image from Pinterest"
         />
 
-        <div>
-          <p className="font-mono w-[50%] animate-rainbow-text">
+<Image
+          className="rounded-xl shadow-2xl shadow-yellow-200"
+          src={`https://i.pinimg.com/564x/7f/d8/ea/7fd8ea6d1ce40c5e9ee037f6b73ea189.jpg`}
+          width={250}
+          height={250}
+          draggable={false}
+          alt="Image from Pinterest"
+        />
+
+
+<Image
+          className="rounded-xl shadow-2xl shadow-yellow-200"
+          src={`https://i.pinimg.com/236x/fd/6f/bc/fd6fbc3b7b28e1910273305ccde9877a.jpg`}
+          width={250}
+          height={250}
+          draggable={false}
+          alt="Image from Pinterest"
+        />
+
+
+<Image
+          className="rounded-xl shadow-2xl shadow-yellow-200"
+          src={`https://i.pinimg.com/236x/c5/27/a5/c527a51f921f1cf59c0decdd16104268.jpg`}
+          width={250}
+          height={250}
+          draggable={false}
+          alt="Image from Pinterest"
+        />
+
+
+
+
+        
+
+       
+      </aside>
+
+      <div className=" flex text-center w-[100%] m-auto items-center " >
+          <p className="font-mono  animate-rainbow-text">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae corporis, quaerat ea accusantium soluta consequuntur voluptates incidunt, voluptatum voluptas in minima maxime molestiae earum veniam sunt consequatur voluptatibus suscipit doloremque.
           </p>
         </div>
-      </aside>
 
       <style jsx>{`
         @keyframes rainbow {
